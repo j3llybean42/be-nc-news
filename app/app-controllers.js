aa7885e898bdb499a9b301917fd5011f49fea301
Ashe <ashemarsh42@gmail.com>
2024-01-15 14:50:30 +0000

    work on task 3

--- app/app-controllers.js
+++ app/app-controllers.js
@@ -1,7 +1,15 @@
-const { selectTopics } = require("./app-models")
+const { selectTopics, selectEndpoints } = require("./app-models")
 
 
 exports.getTopics = (req, res) => {
     selectTopics()
     .then((topics) => res.status(200).send({topics}))
+}
+
+exports.getEndpoints = (req, res) => {
+    selectEndpoints()
+    .then((endpoints) => {
+        console.log(endpoints, "<-- controller endpoints")
+        res.status(200).send({endpoints})
+    })
 }
--- app/app-models.js
+++ app/app-models.js
@@ -1,6 +1,15 @@
 const db = require("../db/connection")
+const fs = require("fs/promises")
 
 exports.selectTopics = () => {
     return db.query(`SELECT * FROM topics;`).then((result) => result.rows)
+}
+
+exports.selectEndpoints = () => {
+    console.log(__dirname)
+    return fs.readFile(`${__dirname}/../endpoints.json`, "utf-8").then((fileContents) => {
+        const endpoints = JSON.parse(fileContents)
+        return endpoints
+    })
 }
 
