7a9beaa49e984f70ffd662ca19a6f6c114091a36
Ashe <ashemarsh42@gmail.com>
2024-02-05 10:18:46 +0000

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const { badPath } = require("./controllers/app.controllers");
 const apiRouter = require("./routes/api-router");
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
